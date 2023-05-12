d2bb9c322bc036cafc30edbc6de0fa1606a63563
Gerson Hernández <gersonhdz8@gmail.com>
2023-05-12 17:27:28 -0500

    Segundo arreglo

--- public/modClientes/clientes.js
+++ public/modClientes/clientes.js
@@ -1,11 +1,20 @@
 //SE guarda toda la informacion del formulario y con los diferentes botones interactua con la información para su manipulación requerida
 let myForm = document.querySelector("#form");
-let listado= document.getElementById("listado");
+let myForm2 = document.querySelector("#form2");
+
 myForm.addEventListener("submit", (e)=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));    
     opc[e.submitter.dataset.accion](data)    
 })
+
+
+myForm2.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));    
+    opc[e.submitter.dataset.accion](data)    
+})
+
 
 const opc = {
     "GET": () => getUserAll(),
@@ -44,9 +53,52 @@ const deleteUser = async(data)=>{
 }
 const searchUser = async(data)=>{
     config.method = "GET";
-    let res = await ( await fetch(`http://localhost:4009/clientes?q=${Object.values(data).join("")}`,config)).json();
-    let info= document.createElement("p");
-    listado.appendChild(info);
-    info.innerHTML=`${Object.fromEntries(res)}`
+    let res = await ( await fetch(`http://localhost:4009/clientes?q=${Object.values(data).join("")}`,config)).json();    
     console.log(res);
+    let tableUsuarios= document.getElementById("tableUsuarios");
+    let infoUsuarios = ""
+
+    if(res.length > 0)
+    {
+        for( let i=0; i<res.length; i++)
+        {
+            
+            infoUsuarios += ` <tr>
+            <td class="text-center border border-slate-300 p-5">${res[i]["documento"]}</td>
+            <td class="text-center border border-slate-300 p-5">${res[i]["nombre"]}</td>
+            <td class="text-center border border-slate-300 p-5">${res[i]["apellido"]}</td>
+            <td class="text-center border border-slate-300 p-5">${res[i]["telefono"]}</td>
+            <td class="text-center border border-slate-300 p-5">${res[i]["fechaNacimiento"]}</td>
+            <td class="text-center border border-slate-300 p-5">${res[i]["ciudad"]}</td>
+            <td class="text-center border border-slate-300 p-5">${res[i]["pais"]}</td>
+            <td class="text-center border border-slate-300 p-5">${res[i]["correo"]}</td>
+            </tr>`
+            tableUsuarios.innerHTML=infoUsuarios
+
+        }
+        
+    }
+    else 
+    {
+        infoUsuarios += ` <tr>
+            <td class="text-center border border-slate-300 p-5">xx</td>
+            <td class="text-center border border-slate-300 p-5">xx</td>
+            <td class="text-center border border-slate-300 p-5">xx</td>
+            <td class="text-center border border-slate-300 p-5">xx</td>
+            <td class="text-center border border-slate-300 p-5">xx</td>
+            <td class="text-center border border-slate-300 p-5">xx</td>
+            <td class="text-center border border-slate-300 p-5">xx</td>
+            <td class="text-center border border-slate-300 p-5">xx</td>
+            
+            </tr>`
+            tableUsuarios.innerHTML=infoUsuarios
+
+    }
+
+
+    
+    
+
+    //info.innerHTML=`${}`
+    //console.log(res);
 }
